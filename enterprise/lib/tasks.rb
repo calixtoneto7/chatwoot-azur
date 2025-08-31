940ddb30c0342e23a6f8c5bcfd9aa6494e006560
Calixto <calixtoneto7@gmail.com>
2025-08-31 18:57:58 -0300

    fix: arquivo tasks.rb

--- enterprise/lib/tasks.rb
+++ enterprise/lib/tasks.rb
@@ -1,4 +1,6 @@
-# Load all rake tasks from the enterprise/lib/tasks directory
-module Tasks
+# lib/tasks/tasks.rb
+
+# Só carrega as tarefas se estivermos rodando via rake
+if defined?(Rake) || File.basename($PROGRAM_NAME) == 'rake'
   Dir.glob(File.join(File.dirname(__FILE__), 'tasks', '*.rake')).each { |r| load r }
 end
